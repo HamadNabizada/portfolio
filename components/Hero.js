838b785fed806f05e8bad66d7a281f3b9c53dcea
Hamad <HamadNabizada@gmail.com>
2022-06-02 21:08:16 -0700

    updated links to go to correct url

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import style from '../styles/Hero.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import Link from 'next/link'
 
 export default function Hero(){
 
@@ -9,13 +10,12 @@ export default function Hero(){
             <h1 className={style.h1Hero}>Hamad Nabizada</h1>
             <h2 className={style.h2Hero}>Frontend Developer</h2>
             <div className={style.links}>
-                <button className={style.btn}>Resume</button>
-                <FontAwesomeIcon className={style.email} icon={faEnvelope}/>
-                <img className={style.image} src="/images/githubLogo.png" alt="Github Logo" />
-                <img className={style.image} src="/images/linkedin.png" alt="Linkedin Logo" />
+                <a href='#'><button className={style.btn}>Resume</button></a>
+                <a href='#'><FontAwesomeIcon className={style.email} icon={faEnvelope}/></a>
+                <a href='https://github.com/HamadNabizada'><img className={style.image} src="/images/githubLogo.png" alt="Github Logo" /></a>
+                <a href='https://www.linkedin.com/in/hamad-nabizada/'><img className={style.image} src="/images/linkedin.png" alt="Linkedin Logo" /></a>
             </div>
             <p className={style.pHero}>I implement user interface design and solve user problems with HTML, CSS AND Javascript. I have 2 years of making products that solve user problems and implementing responsive website.</p>
-
         </div>
 
     )
