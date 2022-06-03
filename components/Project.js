0cdb8993e06df42132cc4a0be17e82c8b489a9d8
Hamad <HamadNabizada@gmail.com>
2022-06-03 07:33:51 -0700

    project component styled.

--- components/Project.js
+++ components/Project.js
@@ -2,11 +2,21 @@ import style from '../styles/Project.module.css'
 import {useState} from 'react'
 
 export default function Project(props){
+    let [isHovered, setIsHovered] = useState(false)
+    function hoverInEvent(){
+        setIsHovered(true)
+    }
+    function hoverOutEvent(){
+        setIsHovered(false)
+    }
+    let myOpacity= isHovered ? {opacity:'2'}: {opacity:'0'}
+    let backgroundOpacity= isHovered ? style.wrapper2Hover : style.wrapper2
 
     return (
-        <div className={style.wrapper} >
-            <button className={style.btn}>View live site</button>
-            <button className={style.btn}>View github code</button>
+        <div onMouseOver={hoverInEvent} onMouseOut={hoverOutEvent} className={style.wrapper} >
+            <div className={backgroundOpacity}></div>
+            <button style={myOpacity} className={style.btn}>View live site</button>
+            <button style={myOpacity} className={style.btn}>View github code</button>
         </div>
     )
 }
